90352f9d9ff212c8c62f0ee54b2d1ed222219085
Vladas <146100@gmail.com>
2020-01-19 19:59:41 +0300

    maxcos fix

--- source/gl_wrapper/GLWrapper.cpp
+++ source/gl_wrapper/GLWrapper.cpp
@@ -54,7 +54,7 @@ void GL::initialize(const gm::Size& size) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL
-#ifdef MAC_OS
+#ifdef APPLE
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
 #endif
 
