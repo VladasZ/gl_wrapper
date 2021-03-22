0e130fd06475806eb6a784dc5d0d25d35dccedbe
Vladas Zakrevskis <146100@gmail.com>
2021-03-22 05:26:15 +0300

    cleanup

--- source/gl_wrapper/buffer/Buffer.cpp
+++ source/gl_wrapper/buffer/Buffer.cpp
@@ -22,7 +22,7 @@ Buffer::Buffer(const BufferConfiguration& configuration, const Buffer::Array<flo
     :
     vertices_data(vertices_data),
     indices(indices),
-    vertices_count(indices.size() == 0 ? vertices_data.size() / configuration.size() : -1) ,
+    vertices_count(indices.empty() ? vertices_data.size() / configuration.size() : -1) ,
     configuration(configuration)
     {
         draw_mode = GL_TRIANGLES;
